a3f2440c7a82a8a656b64e204045dc03f541c217
Ziv Ventura <ziv.ventura@nielsen.com>
2017-09-03 19:30:49 +0300

    Basic layout

--- src/js/Main/Right/Right.js
+++ src/js/Main/Right/Right.js
@@ -1,12 +1,21 @@
 import React from "react"
+import {observer} from 'mobx-react'
 
+
+@observer
 export default class Right extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todoInput: ''
+    }
+  }
   render() {
     return (
       <div>
         <h1>Right</h1>
         <div className="mobx-demo__right-controls">
-          <input type="text"/>
+          <input type="text" value={this.state.todoInput} onChange={(e)=>this.setState({todoInput: e.target.value})}/>
           <button>Add</button>
         </div>
       </div>
